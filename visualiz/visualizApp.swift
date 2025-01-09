8067c53b3cc128f4bfd485511b30f178460e62b6
Lizter Angelo Abrau <lmabrau@up.edu.ph>
2025-01-10 02:31:58 +0800

    Initial Commit

---
+++ visualiz/visualizApp.swift
@@ -0,0 +1,17 @@
+//
+//  visualizApp.swift
+//  visualiz
+//
+//  Created by Angel Abrau on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct visualizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ visualizTests/visualizTests.swift
@@ -0,0 +1,17 @@
+//
+//  visualizTests.swift
+//  visualizTests
+//
+//  Created by Angel Abrau on 1/10/25.
+//
+
+import Testing
+@testable import visualiz
+
+struct visualizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
